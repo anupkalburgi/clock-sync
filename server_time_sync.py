93a218f7fde6f0d12f83b4ccf409b007003c8fa3
Anup Kalburgi(MAC) <anupkalburgi@gmail.com>
2014-11-03 21:44:54 -0500

    Removed the client address by  mistake

--- server_time_sync.py
+++ server_time_sync.py
@@ -19,7 +19,7 @@ class UDPHandler(SocketServer.BaseRequestHandler):
         seq =  data[0]
         client_time_stamp = data[1]
         #pay_load = seq + " " +  client_time_stamp + " " + req_time + " " + '%f' % time.time()
-        socket.sendto( seq + " " +  client_time_stamp + " " + req_time + " " + '%f' % time.time() )
+        socket.sendto( seq + " " +  client_time_stamp + " " + req_time + " " + '%f' % time.time() , self.client_address)
 
 if __name__ == "__main__":
     HOST, PORT = "localhost", 9999
